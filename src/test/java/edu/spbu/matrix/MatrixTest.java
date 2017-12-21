826a6ed584bc199d29e35027bdaef91299d3b972
SergeyMurskiy <31772372+SergeyMurskiy@users.noreply.github.com>
2017-12-22 03:10:45 +0700

    Update MatrixTest.java

--- src/test/java/edu/spbu/matrix/MatrixTest.java
+++ src/test/java/edu/spbu/matrix/MatrixTest.java
@@ -1,19 +1,51 @@
 package edu.spbu.matrix;
 
+
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
-public class MatrixTest
-{
-  /**
-   * ожидается 4 таких теста
-   */
+public class MatrixTest {
   @Test
-  public void mulDD() {
-    Matrix m1 = new DenseMatrix("m1.txt");
-    Matrix m2 = new DenseMatrix("m2.txt");
-    Matrix expected = new DenseMatrix("result.txt");
-    assertEquals(expected, m1.mul(m2));
+  public void mulDD() throws Exception {
+    Matrix A = new DenseMatrix("m1.txt");
+    Matrix B = new DenseMatrix("m2.txt");
+    Matrix D = new DenseMatrix("m3.txt");
+    assertEquals(D, A.mul(B));
+  }
+
+  @Test
+  public void mulSS() throws IOException {
+    Matrix A = new SparseMatrix("m1.txt");
+    Matrix B = new SparseMatrix("m2.txt");
+    Matrix D = new SparseMatrix("m3.txt");
+    assertEquals(D, A.mul(B));
+  }
+
+  @Test
+  public void mulDS() throws IOException {
+    Matrix A = new SparseMatrix("m1.txt");
+    Matrix B = new SparseMatrix("m2.txt");
+    Matrix D = new DenseMatrix("m3.txt");
+    assertEquals(D, A.mul(B));
+  }
+
+  @Test
+  public void mulSD() throws IOException {
+    Matrix A = new SparseMatrix("m1.txt");
+    Matrix B = new DenseMatrix("m2.txt");
+    Matrix D = new DenseMatrix("m3.txt");
+    assertEquals(D, A.mul(B));
+  }
+
+  @Test
+  public void dmulDD() throws Exception {
+    Matrix A = new DenseMatrix("m1.txt");
+    Matrix B = new DenseMatrix("m2.txt");
+    Matrix D = new DenseMatrix("m3.txt");
+    assertEquals(D, A.dmul(B));
   }
 }
+
